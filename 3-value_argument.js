5217696e6535e8262ea30abbfbfee16738f25ede
angelnyambi <angel_nyambi@outlook.com>
2025-10-21 20:09:48 +0200

    edit code

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,7 +3,7 @@ const argument = process.argv[2];
 if (argument === undefined) {
   console.log("No argument");
 } else if (argument) {
-  console.log(HBTN");
+  console.log("HBTN");
 } else {
   console.log("HBTN cool");
 }
