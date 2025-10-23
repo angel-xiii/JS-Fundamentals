97bdaac669cf47408a737c0cda1f726d61efa5f6
angelnyambi <angel_nyambi@outlook.com>
2025-10-23 18:37:35 +0200

    fix code

--- 8-square.js
+++ 8-square.js
@@ -1,6 +1,8 @@
 const arg = ProcessingInstruction.argv[2];
+
 const size = Number.parseInt(arg);
-for (Number.isNaN(size)) {
+
+if (Number.isNaN(size)) {
   console.log("Missing size");
 } else {
     for (let i = 0; i < size; i++) {
